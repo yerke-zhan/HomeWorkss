2fb23d5ba575504e55b82395eebb31b5844bb3d9
Yerke <yerkezhanm@gmail.com>
2024-10-31 13:37:58 +0500

    added homework files

---
+++ hw3-week9-JS/js/script.js
@@ -0,0 +1,16 @@
+var name = "Али" ; 
+console.log( "Аты: " + name + " | Типі: " + typeof name); //sting
+var age = 25;
+console.log("Жасы:"  + age + " | Типі: " +  (typeof age)); //number
+
+let isStudent = true; 
+
+console.log("Студент пе? : " + isStudent +  " | Типі:" + typeof isStudent ) ; //Boolean
+
+age = age.toString() //string
+
+console.log("Жасы (жол ретінде):" + age +  "| Типі: " + typeof age); //sting
+
+
+
+
