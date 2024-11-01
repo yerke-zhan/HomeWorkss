98b44dc1ad61a53122cb876803c7b21e8b2795cb
Yerke <yerkezhanm@gmail.com>
2024-11-01 18:22:37 +0500

    Last HW

--- hw4-week9-JS/js/script.js
+++ hw4-week9-JS/js/script.js
@@ -1,6 +1,6 @@
 var num1 = Number(prompt("Бірінші санды енгізіңіз:"))
-var num2 = Number( prompt("Екінші санды енгізіңіз:"))
-var num3 = Number( prompt("Үшінші санды енгізіңіз:"))
+var num2 = Number(prompt("Екінші санды енгізіңіз:"))
+var num3 = Number(prompt("Үшінші санды енгізіңіз:"))
 
 let Average = (num1 + num2 + num3)/3;
 
