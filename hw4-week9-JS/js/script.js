26176e6a1bdf004763382b2d69f56a5b255e4b59
Yerke <yerkezhanm@gmail.com>
2024-11-01 17:31:18 +0500

    Average

---
+++ hw4-week9-JS/js/script.js
@@ -0,0 +1,7 @@
+var num1 = Number(prompt("Бірінші санды енгізіңіз:"))
+var num2 = Number( prompt("Екінші санды енгізіңіз:"))
+var num3 = Number( prompt("Үшінші санды енгізіңіз:"))
+
+let Average = (num1 + num2 + num3)/3;
+
+alert("Енгізілген сандардың орташа арифметикалық мәні:=" + Average);
