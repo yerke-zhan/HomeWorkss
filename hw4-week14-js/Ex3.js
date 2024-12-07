4875fb9a795d2ef6f9d16b6755bf6821dc6692a2
Yerke <yerkezhanm@gmail.com>
2024-12-08 01:53:53 +0500

    Exercise -3

---
+++ hw4-week14-js/Ex3.js
@@ -0,0 +1,33 @@
+
+let tapsyrmalar =[ "Exercice -1", "Жаттығу-4", "Reading", "Essay"]
+
+let oryndalgan =[]
+let oryndalmagan =[]
+
+
+function askHomework(){
+
+for (let i = 0; i<tapsyrmalar.length; i++){
+    let check = prompt(i+1+ `. Did you do the following homework? ${tapsyrmalar[i]}. \nEnter a number if yes, or anything else if no.` );
+
+if (!isNaN (check) && check!== ""){
+oryndalgan.push (tapsyrmalar[i]);
+alert("Your answer has been added to the answered homework list.");
+
+}
+else {
+    oryndalmagan.push(tapsyrmalar[i]);
+    
+    alert("Your answer has been added to the NOT answered homework list.");
+}
+alert("Answered Homework:"+oryndalgan);
+console.log("Not answered Homework:", oryndalmagan);
+
+}
+
+}
+
+askHomework();
+
+
+
