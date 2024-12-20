d37bd7ef3a2e6a7c9ac5a959dbe797c52de1864d
Yerke <yerkezhanm@gmail.com>
2024-12-20 15:23:22 +0500

    corrected

--- hw-1-week15-js/day2dom.js
+++ hw-1-week15-js/day2dom.js
@@ -1,23 +1,23 @@
 
-let title =document.querySelector('title');
-title.textContent ='HW:Introduction to DOM.';
-console.log(title.textContent);
+let taqyryp =document.querySelector('h1');
+taqyryp.textContent ='HW:Introduction to DOM';
+console.log(taqyryp.textContent);
 
 let aty = document.getElementById('name')
 aty.textContent ='Еркежан апай'
 console.log(aty.textContent);
 
-let hobby1 = document.getElementsByClassName('hobby1')
-hobby1.innerHtml = 'li class="hobby1"><b>Coding</b></li> '
+let hobby1 = document.getElementsByClassName('hobby1');
+hobby1[0].innerHTML = '<b>Coding</b>';
 
-console.log(hobby1.innerHtml);
+console.log(hobby1);
 let hobby2 = document.getElementsByClassName('hobby2')
-hobby2.innerHtml = 'li class="hobby2"><b>Teaching</b></li> '
+hobby2[0].innerHTML = '<b>Teaching</b>'
 
 console.log(hobby2.innerHtml);
 
 let hobby3 = document.getElementsByClassName('hobby3')
-hobby3.innerHtml = 'li class="hobby3"><b>Reading</b></li> '
+hobby3[0].innerHTML = '<b>Reading</b>'
 
 console.log(hobby3.innerHtml);
 
