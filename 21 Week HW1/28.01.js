b75b42d9bc4a0617e5b9f3f048a328bf6a9b868a
Yerke <yerkezhanm@gmail.com>
2025-01-28 23:42:32 +0500

    HW 28.01

---
+++ 21 Week HW1/28.01.js
@@ -0,0 +1,31 @@
+let savebtn = document.getElementById('savebtn')
+let clearBtn = document.getElementById('clearbtn')
+
+let derek =[]
+
+savebtn.addEventListener('click', function(e) {
+    e.preventDefault()
+    let note = document.getElementById('text')
+    
+    
+    if(note){
+      derek.push({note: note.value})
+      console.log(derek);
+
+      localStorage.setItem('derek', JSON.stringify(derek))
+      note.value = ''
+      
+    }
+})
+  
+ 
+ clearBtn.addEventListener('click', function(e) {
+    localStorage.clear()
+    window.location.reload() //обновление страницы
+    console.log('Data cleared successfully')
+ })
+
+
+
+
+
